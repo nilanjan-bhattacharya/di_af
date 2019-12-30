95d23a8cc08dc29558f3e145b04f4f6caa963812
bhattnil <bhattnil@WL355037>
2019-12-30 12:46:17 +0530

    DI using AutoFac

---
+++ DependencyInjectionDemo/ConsoleUI/Application.cs
@@ -0,0 +1,23 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public class Application : IApplication
+    {
+        IBusinessLogic _businessLogic;
+
+        public Application(IBusinessLogic businessLogic)
+        {
+            _businessLogic = businessLogic;
+        }
+
+
+        public void Run()
+        {
+            _businessLogic.ProcessData();
+        }
+    }
+}
---
+++ DependencyInjectionDemo/ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var container = ContainerConfig.Configure(); //Instantiate DI container.
+
+            using var scope = container.BeginLifetimeScope();
+            var app = scope.Resolve<IApplication>();
+            app.Run();
+
+            Console.ReadLine();
+        }
+    }
+}
